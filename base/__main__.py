27a763d8f7984815ac65bd19e201623c7716c6e2
Mostafa Motahari <mostafamotahari2004@gmail.com>
2021-08-19 10:27:11 +0200

    Creating first files and installing necessary requirements.

---
+++ base/__main__.py
@@ -0,0 +1,10 @@
+"""The main file of this bot.
+Please replace your information in api ids and bot token in config file.
+"""
+
+# Imports
+from pyrogram import Client
+
+app = Client("google")
+
+app.run()
