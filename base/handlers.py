76b781aea212c56973baca7eaffd839c41046412
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-01-22 14:21:55 -0500

    Create registration handlers.

--- base/handlers.py
+++ base/handlers.py
@@ -3,7 +3,7 @@ from telegram import Update
 from telegram.ext import CallbackContext
 
 from plugins import google_search
-from session import get_db
+from session import get_db, TEMP_DATA
 from db_methods import (
     get_users_list,
     sign_up,
@@ -15,35 +15,41 @@ import messages
 
 # Handlers
 
+# Check user verfing
+def verify_user(update: Update, user_id: int):
+    global TEMP_DATA
+    if user_id not in TEMP_DATA:
+        update.message.reply_text(
+            messages.signup_failed
+        )
+        return False
+
+    return True
+
+
 # Start Message
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        messages.start_msg
-    )
-    db = get_db().__next__()
     user_id = update.message.from_user.id
-    users_list = get_users_list(db)
-
-    if user_id not in users_list:
-        sign_up(db, user_id)
+    
+    if verify_user(update, user_id):
+        update.message.reply_text(
+            messages.start_msg
+        )
 
 
 # Searching Image
 def search_image(update: Update, context: CallbackContext):
+
+    db = get_db().__next__()
+    user_id = update.message.from_user.id
+    
+    if not verify_user(update, user_id):
+        return 0        
+
     google_search(
         update.message.photo[0].get_file().file_path,
         update.message
     )
-
-    db = get_db().__next__()
-    user_id = update.message.from_user.id
-    users_list = get_users_list(db)
-
-    if user_id not in users_list:
-        sign_up(db, user_id, first_search=True)
-
-        update.message.reply_text(messages.not_updated_msg)
-        return 1
 
     increase_search(db, user_id)
 
@@ -113,4 +119,18 @@ def public_message(update: Update, context: CallbackContext):
 
     update.message.reply_text(
         "پیام همگانی با موفقیت ارسال شد!"
+    )
+
+
+# Signup a user by admin
+def signup_by_admin(update: Update, context: CallbackContext):
+    db = get_db().__next__()
+
+    target_user_id = int(
+        re.findall("Id: (.*)", update.message.text)[0]
+    )
+    sign_up(db, target_user_id)
+
+    update.message.reply_text(
+        "کابر مورد نظر با موفقیت ثبت نام شد!✅"
     )
